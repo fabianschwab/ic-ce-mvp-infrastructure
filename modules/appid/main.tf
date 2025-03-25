ffb3e9a619aa9861cf2c9fd853fb5102abd8cdfb
Fabian Schwab <fabian.schwab@ibm.com>
2025-03-25 09:54:51 +0100

    basic app id config

--- modules/appid/main.tf
+++ modules/appid/main.tf
@@ -11,3 +11,34 @@ resource "ibm_resource_key" "appid_credentials" {
   role                 = "Writer"
   resource_instance_id = ibm_resource_instance.appid.id
 }
+
+resource "ibm_appid_idp_cloud_directory" "cd" {
+  tenant_id = ibm_resource_instance.appid.guid
+  is_active = true
+  identity_confirm_methods = [
+    "email"
+  ]
+  identity_field                      = "email"
+  self_service_enabled                = false
+  signup_enabled                      = false
+  welcome_enabled                     = false
+  reset_password_enabled              = false
+  reset_password_notification_enabled = false
+}
+
+resource "ibm_appid_idp_facebook" "fb" {
+  tenant_id = ibm_resource_instance.appid.guid
+  is_active = false
+}
+resource "ibm_appid_idp_google" "gg" {
+  tenant_id = ibm_resource_instance.appid.guid
+  is_active = false
+}
+resource "ibm_appid_idp_saml" "saml" {
+  tenant_id = ibm_resource_instance.appid.guid
+  is_active = false
+}
+resource "ibm_appid_idp_custom" "idp" {
+  tenant_id = ibm_resource_instance.appid.guid
+  is_active = false
+}
