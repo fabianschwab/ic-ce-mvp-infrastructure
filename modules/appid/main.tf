d8d44f0a53b1a60460a4ebfa740c0c2a4aa48794
Fabian Schwab <fabian.schwab@ibm.com>
2025-03-25 09:05:26 +0100

    appid secrets

--- modules/appid/main.tf
+++ modules/appid/main.tf
@@ -5,3 +5,9 @@ resource "ibm_resource_instance" "appid" {
   location          = var.region
   resource_group_id = var.resource_group_id
 }
+
+resource "ibm_resource_key" "appid_credentials" {
+  name                 = "appid-service-credentials"
+  role                 = "Writer"
+  resource_instance_id = ibm_resource_instance.appid.id
+}
