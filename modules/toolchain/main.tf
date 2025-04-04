4af2397c297803038ce524c02cdfe3af72485ff6
Fabian Schwab <fabian.schwab@ibm.com>
2025-04-04 14:09:07 +0200

    fix for hosting pipeline or catalog definitions on other git servers

--- modules/toolchain/main.tf
+++ modules/toolchain/main.tf
@@ -1,6 +1,19 @@
 # -----------------------------------------------------------
 # ------------------------ Toolchain ------------------------
 # -----------------------------------------------------------
+locals {
+  pipeline_git_id = (
+    can(regex("^https://github\\.com/", var.repository_url_pipeline)) ? "github" :
+    can(regex("^https://github\\.ibm\\.com/", var.repository_url_pipeline)) ? "integrated" :
+    "githubcustom"
+  )
+  catalog_git_id = (
+    can(regex("^https://github\\.com/", var.repository_url_pipeline_catalog)) ? "github" :
+    can(regex("^https://github\\.ibm\\.com/", var.repository_url_pipeline_catalog)) ? "integrated" :
+    "githubcustom"
+  )
+}
+
 resource "ibm_cd_toolchain" "ci_cd_toolchain" {
   name              = "code-engine-deployment"
   resource_group_id = var.resource_group_id
@@ -11,10 +24,12 @@ resource "ibm_cd_toolchain_tool_githubconsolidated" "tekton_repository" {
 
   toolchain_id = ibm_cd_toolchain.ci_cd_toolchain.id
   initialization {
+    git_id   = local.pipeline_git_id
     type     = "link"
     repo_url = var.repository_url_pipeline
   }
   parameters {
+    git_id   = local.pipeline_git_id
     repo_url = var.repository_url_pipeline
   }
 }
@@ -22,10 +37,12 @@ resource "ibm_cd_toolchain_tool_githubconsolidated" "tekton_catalog" {
 
   toolchain_id = ibm_cd_toolchain.ci_cd_toolchain.id
   initialization {
+    git_id   = local.catalog_git_id
     type     = "link"
     repo_url = var.repository_url_pipeline_catalog
   }
   parameters {
+    git_id   = local.catalog_git_id
     repo_url = var.repository_url_pipeline_catalog
   }
 }
