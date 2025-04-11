15b4a641989a80ed5dc75bef0714708ab4d4464b
Fabian Schwab <fabian.schwab@ibm.com>
2025-04-11 12:35:34 +0200

    github

--- modules/toolchain/main.tf
+++ modules/toolchain/main.tf
@@ -1,18 +1,18 @@
 # -----------------------------------------------------------
 # ------------------------ Toolchain ------------------------
 # -----------------------------------------------------------
-# locals {
-#   pipeline_git_id = (
-#     can(regex("^https://github\\.com/", var.repository_url_pipeline)) ? "github" :
-#     can(regex("^https://github\\.ibm\\.com/", var.repository_url_pipeline)) ? "integrated" :
-#     "githubcustom"
-#   )
-#   catalog_git_id = (
-#     can(regex("^https://github\\.com/", var.repository_url_pipeline_catalog)) ? "github" :
-#     can(regex("^https://github\\.ibm\\.com/", var.repository_url_pipeline_catalog)) ? "integrated" :
-#     "githubcustom"
-#   )
-# }
+locals {
+  pipeline_git_id = (
+    can(regex("^https://github\\.com/", var.repository_url_pipeline)) ? "github" :
+    can(regex("^https://github\\.ibm\\.com/", var.repository_url_pipeline)) ? "integrated" :
+    "githubcustom"
+  )
+  catalog_git_id = (
+    can(regex("^https://github\\.com/", var.repository_url_pipeline_catalog)) ? "github" :
+    can(regex("^https://github\\.ibm\\.com/", var.repository_url_pipeline_catalog)) ? "integrated" :
+    "githubcustom"
+  )
+}
 
 resource "ibm_cd_toolchain" "ci_cd_toolchain" {
   name              = "code-engine-deployment"
@@ -24,12 +24,12 @@ resource "ibm_cd_toolchain_tool_githubconsolidated" "tekton_repository" {
 
   toolchain_id = ibm_cd_toolchain.ci_cd_toolchain.id
   initialization {
-    git_id   = "github"
+    git_id   = local.pipeline_git_id
     type     = "link"
     repo_url = var.repository_url_pipeline
   }
   parameters {
-    git_id   = "github"
+    git_id   = local.pipeline_git_id
     repo_url = var.repository_url_pipeline
   }
 }
@@ -37,12 +37,12 @@ resource "ibm_cd_toolchain_tool_githubconsolidated" "tekton_catalog" {
 
   toolchain_id = ibm_cd_toolchain.ci_cd_toolchain.id
   initialization {
-    git_id   = "github"
+    git_id   = local.catalog_git_id
     type     = "link"
     repo_url = var.repository_url_pipeline_catalog
   }
   parameters {
-    git_id   = "github"
+    git_id   = local.catalog_git_id
     repo_url = var.repository_url_pipeline_catalog
   }
 }
